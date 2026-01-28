ec3db621dfb4341e90d69cfb770df9e0bc38ff83
John F Hoeppner <johncom2k@gmail.com>
2026-01-28 10:34:28 +0700

    Update credentials.h

--- demo-projects/watt_iz_files/src/credentials.h
+++ demo-projects/watt_iz_files/src/credentials.h
@@ -8,8 +8,10 @@
  #define WIFI_SSID          "your_wifi_ssid"            // PRIVATE!
  #define WIFI_PASSWORD      "your_wifi_password"        // PRIVATE!
  #define GOOGLE_SERVER      "speech.googleapis.com"
- #define GOOGLE_ENDPOINT    "https://texttospeech.googleapis.com/v1/text:synthesize?key="
+ #define GOOGLE_TTS_ENDPOINT "https://texttospeech.googleapis.com/v1/text:synthesize?key="
+ #define GOOGLE_STT_ENDPOINT "https://speech.googleapis.com/v1/speech:recognize?key="
+ #define GOOGLE_TRANSLATE_ENDPOINT "https://translation.googleapis.com/language/translate/v2?key=" 
  #define GOOGLE_API_KEY     "your_google_api_key"       // PRIVATE!
  #define OPENAI_ENDPOINT    "https://api.openai.com/v1/chat/completions"
  #define OPENAI_API_KEY     "your_openai_api_key"       // PRIVATE!
- #define OPENAI_CHAT_MODEL  "gpt-4.1-mini"
+ #define OPENAI_CHAT_MODEL  "gpt-4.1"
